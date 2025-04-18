d9bcf1ed08baed8a6cc0fd4eb4f681ae42e7b00f
sofiberry <sofi2darling@gmail.com>
2025-04-18 10:09:01 +0300

    add dark theme option

--- components/explore-dropdown.tsx
+++ components/explore-dropdown.tsx
@@ -83,27 +83,27 @@ export function ExploreDropdown() {
 
       {isExploreOpen && (
         <div className="absolute -left-20 sm:-left-16 md:-left-25 lg:-left-10 top-full mt-10 z-50">
-          <div className="bg-white rounded-lg shadow-lg border overflow-hidden flex flex-col sm:flex-row max-w-[80vw] sm:max-w-[80vw] md:max-w-[720px] lg:max-w-[900px]">
+          <div className="bg-white dark:bg-gray-950 rounded-lg shadow-lg border overflow-hidden flex flex-col sm:flex-row max-w-[80vw] sm:max-w-[80vw] md:max-w-[720px] lg:max-w-[900px]">
             {/* Left Column - Categories */}
             <div className="w-[172px] sm:w-[250px] md:w-[300px] max-h-[60vh] sm:max-h-[80vh] overflow-y-auto">
               <div className="font-bold px-3 pt-3 sm:px-4 sm:pt-3">
-                <h3 className="text-[#646464] text-xs sm:text-sm">Browse Certifications</h3>
+                <h3 className="text-[#646464] dark:text-[#9a9a9a] text-xs sm:text-sm lg:text-base">Browse Certifications</h3>
               </div>
               <div className="p-3 sm:px-4 border-b">
-                <h3 className="text-gray-800 text-xs sm:text-sm">Certification preparation</h3>
+                <h3 className="text-gray-800 dark:text-gray-300 text-xs sm:text-sm lg:text-base">Certification preparation</h3>
               </div>
               {categoriesData.map((category, index) => (
                 <div
                   key={index}
-                  className={`flex items-center justify-between text-[9px] sm:text-xs px-3 py-1 sm:px-4 sm:py-3 hover:bg-gray-100 cursor-pointer transition-colors duration-150 ${
-                    category.name === activeCategory ? "bg-gray-50" : ""
+                  className={`flex items-center justify-between text-[9px] sm:text-xs lg:text-sm px-3 py-1 sm:px-4 sm:py-3 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer transition-colors duration-150 ${
+                    category.name === activeCategory ? "bg-gray-50 dark:bg-gray-700" : ""
                   }`}
                   onClick={() => handleCategoryClick(category.name)}
                 >
                   <span
                     className={`${
-                      category.name === activeCategory ? "text-[#2196F3]" : "text-gray-800"
-                    } font-light text-[9px] sm:text-xs`}
+                      category.name === activeCategory ? "text-[#2196F3]" : "text-gray-800 dark:text-gray-300"
+                    } font-light text-[9px] sm:text-xs lg:text-sm 2xl:text-base`}
                   >
                     {category.name}
                   </span>
@@ -133,15 +133,15 @@ export function ExploreDropdown() {
                   ?.subcategories.map((subcategory, index) => (
                     <div
                       key={index}
-                      className={`flex items-center justify-between px-3 py-2 sm:px-4 sm:py-3 hover:bg-gray-100 cursor-pointer transition-colors duration-150 ${
-                        subcategory.name === activeSubcategory ? "bg-gray-50" : ""
+                      className={`flex items-center justify-between px-3 py-2 sm:px-4 sm:py-3 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer transition-colors duration-150 ${
+                        subcategory.name === activeSubcategory ? "bg-gray-50 dark:bg-gray-700" : ""
                       }`}
                       onClick={() => handleSubcategoryClick(subcategory.name)}
                     >
                       <span
                         className={`${
-                          subcategory.name === activeSubcategory ? "text-[#2196F3]" : "text-gray-800"
-                        } font-light text-[9px] sm:text-xs`}
+                          subcategory.name === activeSubcategory ? "text-[#2196F3]" : "text-gray-800 dark:text-gray-300"
+                        } font-light text-[9px] sm:text-xs lg:text-sm 2xl:text-base`}
                       >
                         {subcategory.name}
                       </span>
@@ -168,7 +168,7 @@ export function ExploreDropdown() {
                 </div>
 
                 <div className="px-3 py-2 sm:px-4 sm:py-3 border-b">
-                  <h3 className="text-gray-800 text-xs sm:text-sm">Popular topics</h3>
+                  <h3 className="text-gray-800 dark:text-gray-300 text-xs sm:text-sm lg:text-base">Popular topics</h3>
                 </div>
                 {categoriesData
                   .find((cat) => cat.name === activeCategory)
@@ -177,9 +177,9 @@ export function ExploreDropdown() {
                     <Link
                       href={`/topics/${encodeURIComponent(topic.toLowerCase().replace(/\s+/g, "-"))}`}
                       key={index}
-                      className="block px-3 py-2 sm:px-4  hover:bg-gray-100 transition-colors duration-150"
+                      className="block px-3 py-2 sm:px-4  hover:bg-gray-100 dark:bg-gray-800 transition-colors duration-150"
                     >
-                      <span className="text-gray-800 font-light text-[9px] sm:text-xs">{topic}</span>
+                      <span className="text-gray-800 dark:text-gray-300 font-light text-[9px] sm:text-xs lg:text-sm 2xl:text-base">{topic}</span>
                     </Link>
                   ))}
               </div>
