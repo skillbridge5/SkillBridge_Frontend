f695ff138746e9c6120853ee35b82d3e5fbffe17
sofiberry <sofi2darling@gmail.com>
2025-06-06 17:51:39 +0300

    making the service section component responsive

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -14,7 +14,7 @@ export interface ServicesGridProps {
 
 export function ServicesSection({ services }: ServicesGridProps) {
   return (
-    <section className='py-16 bg-gray-50 dark:bg-gray-900 '>
+    <section className='py-16 bg-[#FBFBFB] dark:bg-gray-900/40 '>
       <div className='container mx-auto px-4'>
         <SectionHeading
           title='Our Services'
@@ -22,11 +22,11 @@ export function ServicesSection({ services }: ServicesGridProps) {
           center={true}
         />
 
-        <div className='grid md:grid-cols-3 gap-6'>
+        <div className='grid md:grid-cols-3 gap-6 md:gap-4 lg:gap-12 xl:gap-16 2xl:gap-24'>
           {services.map((service, index) => (
-            <AnimatedCard key={index} delay={0.1 * index} className='h-full'>
-              <Card className='border-none shadow-md dark:bg-gray-800 h-full transition-all duration-300 hover:shadow-xl hover:bg-[#2196F3] group'>
-                <CardContent className='p-6'>
+            <AnimatedCard key={index} delay={0.1 * index} className=''>
+              <Card className='border-none shadow-md bg-white dark:bg-gray-900 dark:hover:bg-gray-800 h-full transition-all duration-300 hover:shadow-xl hover:bg-[#2196F3] group hover:scale-105 font-montserrat'>
+                <CardContent className='px-6 py-5'>
                   <div className='w-12 h-12 rounded-lg bg-[#E0EAFF] flex items-center justify-center mb-4 group-hover:bg-white transition-colors duration-300'>
                     {service.icon &&
                       React.isValidElement(service.icon) &&
@@ -37,7 +37,7 @@ export function ServicesSection({ services }: ServicesGridProps) {
                   <h3 className='text-xl font-bold mb-2 dark:text-white group-hover:text-white'>
                     {service.title}
                   </h3>
-                  <p className='text-gray-600 dark:text-gray-400 mb-4 group-hover:text-white'>
+                  <p className='text-xs max-[540px]:!text-sm md:!text-base text-gray-600 dark:text-gray-400 mb-4 group-hover:text-white'>
                     {service.description}
                   </p>
                   <Link
