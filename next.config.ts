94aa14712e8c26a2b1b40967a2054116a455d824
sofiberry <sofi2darling@gmail.com>
2025-08-09 13:26:03 +0300

    update configuration of next app

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,38 @@
-import { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "skillbridge.s3.amazonaws.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'skillbridge.s3.amazonaws.com',
       },
       {
-        protocol: "https",
-        hostname: "miro.medium.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'miro.medium.com',
       },
       {
-        protocol: "https",
-        hostname: "i.ibb.co",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'i.ibb.co',
       },
       {
-         protocol: "https",
-         hostname: "img.freepik.com",
-         port: "",
-         pathname: "/**",
+        protocol: 'https',
+        hostname: 'img.freepik.com',
       }
     ],
   },
   async rewrites() {
     return [
       {
-        source: "/api/:path*", // Frontend path (e.g., /api/auth/register-student)
-        destination: "https://skillbridge-backend-w2s4.onrender.com/api/:path*", // Backend URL
+        source: '/api/:path*',
+        destination: 'https://skillbridge-backend-w2s4.onrender.com/api/:path*',
       },
     ];
   },
 };
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
