127c870ccebd6ab07099badfa89cddbf52eb6f01
sofiberry <sofi2darling@gmail.com>
2025-08-09 10:24:39 +0300

    add: a helmet for seo

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,9 +6,30 @@ import { InstructorsSection } from "@/app/[locale]/components/instructors-sectio
 import { TestimonialsSection } from "@/app/[locale]/components/testimonials-section";
 import { SuccessSection } from "@/app/[locale]/components/success-section";
 import Footer from "@/app/[locale]/components/footer";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
   return (
+    <>
+    <Helmet>
+      <title>SkillBridge | Upskill with Expert-Led Courses & Training</title>
+      <meta
+        name="description"
+        content="SkillBridge connects learners with expert instructors for high-quality courses, training, and career advancement. Explore our services and join our success stories."
+      />
+      <meta
+        property="og:title"
+        content="SkillBridge | Upskill with Expert-Led Courses"
+      />
+      <meta
+        property="og:description"
+        content="Join SkillBridge to access top-notch courses, learn from industry-leading instructors, and boost your career with our proven success stories."
+      />
+      <meta property="og:image" content="https://i.ibb.co/ZRYfMLWK/skills.png" />
+      <meta property="og:url" content="https://skill-bridge-iot.vercel.app/" />
+      <meta name="google-site-verification" content="tK8jg7pTludPfa4R8AfjqPslodhRAJbKY1AYdI_z70g" />
+
+    </Helmet>
     <div className='min-h-screen bg-white dark:bg-gray-950 transition-colors duration-300 font-montserrat'>
       <Navbar />
       <HeroSection />
@@ -19,5 +40,6 @@ export default function Home() {
       <SuccessSection />
       <Footer />
     </div>
+    </>
   );
 }
