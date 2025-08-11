c6e96246221c80d4ff6526a0781e49a6b1e2465c
sofiberry <sofi2darling@gmail.com>
2025-08-11 23:42:18 +0300

    update: next configuration

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
       }
     ],
   },
